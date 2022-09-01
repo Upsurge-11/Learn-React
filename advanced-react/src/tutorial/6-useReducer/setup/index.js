08a6793c1db746f7a187e6b68bcf9c332393724d
Upsurge-11 <arghyadeepbumbum@gmail.com>
2022-09-01 22:53:07 +0530

    Changes to be committed: 	modified:   advanced-react/src/tutorial/6-useReducer/setup/Modal.js 	modified:   advanced-react/src/tutorial/6-useReducer/setup/index.js

--- advanced-react/src/tutorial/6-useReducer/setup/index.js
+++ advanced-react/src/tutorial/6-useReducer/setup/index.js
@@ -2,27 +2,39 @@ import React, { useState, useReducer } from "react";
 import Modal from "./Modal";
 import { data } from "../../../data";
 // reducer function
-
+const reducer = (state, action) => {
+  if (action.type === "ADD_ITEM") {
+    const newPeople = [...state.people, action.payload];
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: "Item added"
+    };
+  }
+  throw new Error("No matching action type.");
+};
+// initial state
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: ""
+};
 const Index = () => {
   const [name, setName] = useState("");
-  const [people, setPeople] = useState(data);
-  const [showModal, setShowModal] = useState(false);
+  const [state, dispatch] = useReducer(reducer, defaultState);
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
-      setShowModal(true);
-      setPeople([
-        ...people,
-        { id: new Date(new Date().getTime()).toString(), name },
-      ]);
-      setName("");
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "ADD_ITEM", payload: newItem });
     } else {
-      setShowModal(true);
+      dispatch({ type: "RANDOM" });
     }
   };
   return (
     <>
-      {showModal && <Modal />}
+      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
       <form onSubmit={handleSubmit} className="form">
         <div>
           <input
@@ -33,7 +45,7 @@ const Index = () => {
         </div>
         <button type="submit">add </button>
       </form>
-      {people.map((person) => {
+      {state.people.map((person) => {
         return (
           <div key={person.id}>
             <h4>{person.name}</h4>
