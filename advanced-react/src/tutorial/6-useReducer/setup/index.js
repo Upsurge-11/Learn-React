3b531396dab8510db6a935321899f3c44f47f793
Arghyadeep Mukherjee <arghyadeepbumbum@gmail.com>
2022-09-05 01:19:24 +0530

    useReducer, prop drilling, useContext and custom hooks code added along with future react project course.

--- advanced-react/src/tutorial/6-useReducer/setup/index.js
+++ advanced-react/src/tutorial/6-useReducer/setup/index.js
@@ -1,19 +1,6 @@
 import React, { useState, useReducer } from "react";
 import Modal from "./Modal";
-import { data } from "../../../data";
-// reducer function
-const reducer = (state, action) => {
-  if (action.type === "ADD_ITEM") {
-    const newPeople = [...state.people, action.payload];
-    return {
-      ...state,
-      people: newPeople,
-      isModalOpen: true,
-      modalContent: "Item added"
-    };
-  }
-  throw new Error("No matching action type.");
-};
+import { reducer } from "./reducer";
 // initial state
 const defaultState = {
   people: [],
@@ -28,13 +15,19 @@ const Index = () => {
     if (name) {
       const newItem = { id: new Date().getTime().toString(), name };
       dispatch({ type: "ADD_ITEM", payload: newItem });
+      setName("");
     } else {
-      dispatch({ type: "RANDOM" });
+      dispatch({ type: "NO_VALUE" });
     }
+  };
+  const closeModal = () => {
+    dispatch({ type: "CLOSE_MODAL" });
   };
   return (
     <>
-      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
+      {state.isModalOpen && (
+        <Modal closeModal={closeModal} modalContent={state.modalContent} />
+      )}
       <form onSubmit={handleSubmit} className="form">
         <div>
           <input
@@ -43,12 +36,19 @@ const Index = () => {
             onChange={(e) => setName(e.target.value)}
           />
         </div>
-        <button type="submit">add </button>
+        <button type="submit">Add </button>
       </form>
       {state.people.map((person) => {
         return (
-          <div key={person.id}>
+          <div key={person.id} className="item">
             <h4>{person.name}</h4>
+            <button
+              onClick={() =>
+                dispatch({ type: "REMOVE_ITEM", payload: person.id })
+              }
+            >
+              Remove
+            </button>
           </div>
         );
       })}
